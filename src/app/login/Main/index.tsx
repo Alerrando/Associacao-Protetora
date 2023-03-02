9b7bf61a8868ed756dce58d8cdf7aa14657cc627
Alerrando <alerrando2@gmail.com>
2023-03-01 21:23:50 -0300

    login router complete

--- src/app/login/Main/index.tsx
+++ src/app/login/Main/index.tsx
@@ -1,6 +1,7 @@
 'use client';
 import Image from 'next/image'
-import { PawPrint } from 'phosphor-react'
+import Link from 'next/link';
+import { EnvelopeSimple, FacebookLogo, GoogleLogo, LockKey, PawPrint, TwitterLogo } from 'phosphor-react'
 import { useEffect, useState } from 'react';
 import './style.scss'
 
@@ -20,14 +21,54 @@ export default function Main() {
             </div>
 
             <section className='container-login'>
-                <header className='login-header'>
-                    <PawPrint size={64} />
-                    <h1>Login</h1>
-                </header>
+                <div className='login'>
+                    <form>
+                        <header className='login-header'>
+                            <PawPrint size={64} />
+                            <h1>Login</h1>
+                        </header>
 
-                <form>
+                        <div className="container-form-inputs">
+                            <div className="input">
+                                <EnvelopeSimple size={24} />
+                                <input type="email" name="email" placeholder='Email' />
+                            </div>
 
-                </form>
+                            <div className="input">
+                                <LockKey size={24} />
+                                <input type="senha" name="senha" placeholder='Senha' />
+                            </div>
+
+                            <a href="">Esqueci a senha!</a>
+
+                            <button type='submit'className='btn'>Login</button>
+
+                            <div className="container-login-apps">
+                                <div className="login-apps-text">
+                                    <div className="traco"></div>
+                                    <span>Ou logue por</span>
+                                    <div className="traco"></div>
+                                </div>
+
+                                <div className="login-apps">
+                                    <div className="app">
+                                        <FacebookLogo size={32} className="facebook" />
+                                    </div>
+
+                                    <div className="app">
+                                        <TwitterLogo size={32} className="twitter" />
+                                    </div>
+                                    
+                                    <div className="app">
+                                        <GoogleLogo size={32} className="google" />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <span className='register'>Não tem conta? <Link href="/cadastro">Cadastre-se</Link></span>
+                        </div>
+                    </form>
+                </div>
             </section>
         </main>
     )
